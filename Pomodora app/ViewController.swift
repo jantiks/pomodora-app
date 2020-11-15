b71475d698b9c67306b1a4e65597596701b754e7
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-15 14:38:12 +0400

    Initial Commit

---
+++ Pomodora app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomodora app
+//
+//  Created by Tigran on 11/15/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
