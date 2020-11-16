555721d6955651f56b573e6e860e182f3afe8ffa
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-16 21:19:23 +0400

    added vibration , stop session button but something went wrong with the functionality

--- Pomodora app/ViewController.swift
+++ Pomodora app/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
     @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var stopLabel: UIButton!
     
     var timer: Timer!
     var timeRemaining = 15
@@ -17,32 +19,72 @@ class ViewController: UIViewController {
     var minutes = ""
     var seconds = ""
     var rest = false
+    var isWorking = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         minutes = String(format: "%02d", timeRemaining/60)
         seconds = String(format: "%02d", timeRemaining%60)
         buttonLabel.setTitle("\(minutes):\(seconds)", for: .normal)
         UIView.setAnimationsEnabled(false)
+        stopLabel.setTitle("End Session", for: .normal)
+        stopLabel.isHidden = true
 
     }
-
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
-        if !rest {
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(workStep), userInfo: nil, repeats: true)
-            rest = true
+        stopLabel.isHidden = false
+        if !isWorking{
+            isWorking = true
+            if !rest {
+                rest = true
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(workStep), userInfo: nil, repeats: true)
+                
+            }else {
+                rest = false
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(restStep), userInfo: nil, repeats: true)
+                
+            }
+
         }else {
-             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(restStep), userInfo: nil, repeats: true)
-            rest = false
+            timer.invalidate()
+            isWorking = false
         }
     }
     
+    @IBAction func stopTapped(_ sender: UIButton) {
+        alert()
+        
+    }
+    
+    func alert() {
+        let alert = UIAlertController(title: "End Session", message: "Do you want to end this session?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "YES", style: .default, handler:{ action in
+            self.timer.invalidate()
+            self.timeRemaining = 15
+            self.minutes = String(format: "%02d", self.timeRemaining/60)
+            self.seconds = String(format: "%02d", self.timeRemaining%60)
+            self.buttonLabel.setTitle("\(self.minutes):\(self.seconds)", for: .normal)
+            self.isWorking = false
+            self.stopLabel.isHidden = true
+
+
+        } ))
+        alert.addAction(UIAlertAction(title: "NO", style: .cancel, handler: nil))
+
+        present(alert,animated: true)
+    }
+//
     @objc func workStep() {
         if timeRemaining > 0 {
             timeRemaining -= 1
         }else {
             timer.invalidate()
             timeRemaining = restTime
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
         }
         minutes = String(format: "%02d", timeRemaining/60)
         seconds = String(format: "%02d", timeRemaining%60)
@@ -54,6 +96,7 @@ class ViewController: UIViewController {
         }else {
             timer.invalidate()
             timeRemaining = 15
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
         }
         minutes = String(format: "%02d", timeRemaining/60)
         seconds = String(format: "%02d", timeRemaining%60)
